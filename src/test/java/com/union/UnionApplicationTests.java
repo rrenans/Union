1f95579bd64f6da43a39682e157c29edc061470b
renans04 <renansantos.silva04@outlook.com>
2021-09-09 19:45:35 -0300

    Projeto iniciado

---
+++ src/test/java/com/union/UnionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.union;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
