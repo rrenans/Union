67a22fbe20290ca18e7da7cd7bd2dbd725511bf0
renans04 <renansantos.silva04@outlook.com>
2021-09-09 22:24:28 -0300

    Classe principal resposável pela inicialização do projeto, foi adicionado um pequeno método get

--- src/main/java/com/union/UnionApplication.java
+++ src/main/java/com/union/UnionApplication.java
@@ -2,12 +2,20 @@ package com.union;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UnionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnionApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "Acesse a página http://localhost:8080/funcionarios";
 	}
 
 }
