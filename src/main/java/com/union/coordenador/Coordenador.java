6e5da167bc6589067d92e0782a4b38a5ca69a347
renans04 <renansantos.silva04@outlook.com>
2021-09-15 15:35:58 -0300

    Classes principais atualizadas para uma melhor performace do projeto

--- src/main/java/com/union/coordenador/Coordenador.java
+++ src/main/java/com/union/coordenador/Coordenador.java
@@ -1,7 +1,17 @@
 package com.union.coordenador;
-
+/*
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+*/
+// @Entity
+// @Table(name = "Coordenador")
 public class Coordenador {
 
+	// @Id
+	// @GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
 	// private String email;
