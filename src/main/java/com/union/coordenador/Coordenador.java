17ce4be8aa110343c35dbeadfff5032dc337c656
renans04 <renansantos.silva04@outlook.com>
2021-09-16 13:24:41 -0300

    Arquivos alterados para uma melhor perfomace e seguindo padrão design patterns

--- src/main/java/com/union/coordenador/Coordenador.java
+++ src/main/java/com/union/coordenador/Coordenador.java
@@ -1,24 +1,28 @@
 package com.union.coordenador;
-/*
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-*/
-// @Entity
-// @Table(name = "Coordenador")
+
+@Entity // Entidade para o banco de dados
+@Table(name = "Coordenador") // Mostra que a minha classe coordenador é uma tabela no banco de dados
 public class Coordenador {
 
-	// @Id
-	// @GeneratedValue(strategy = GenerationType.AUTO)
+	@Id // Primary key do banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // seria o Auto increment do nosso banco
 	private Integer id;
+	
+	@Column // Nome é uma coluna
 	private String nome;
 	// private String email;
 	// private String cpf;
 	// private String cep;
 	// private Integer telefone;
 	
+	// pq um construtor vazio é importante?
 	public Coordenador() {
 		super();
 	}
--- src/main/java/com/union/coordenador/CoordenadorController.java
+++ src/main/java/com/union/coordenador/CoordenadorController.java
@@ -1,29 +1,38 @@
 package com.union.coordenador;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/")
 public class CoordenadorController {
 	
-	private List<Coordenador> coordenadores = new ArrayList<>();
+	private final CoordenadorService coordenadorService;
+
+	@Autowired
+	public CoordenadorController(CoordenadorService coordenadorService) {
+		super();
+		this.coordenadorService = coordenadorService;
+	}
 	
 	@GetMapping
-	public List<Coordenador> getCoordenador() {
-		return coordenadores;
+	public List<Coordenador> pegarCoordenadores() {
+		return coordenadorService.pegarCoordenadores();
 	}
 	
 	@PostMapping
-	public Coordenador coordenador(@RequestBody Coordenador coordenador) {
-		coordenadores.add(coordenador);
-		return coordenador;
-	} 
+	@ResponseStatus(HttpStatus.CREATED)
+	public void registrarCoordenador(@RequestBody Coordenador coordenador) {
+		coordenadorService.adicionarNovoCoordenador(coordenador);
+	}
 	
 }
