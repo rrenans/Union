280ec70bbc23ba4562f27410bd40c541fb4995b8
renans04 <renansantos.silva04@outlook.com>
2021-09-16 13:22:49 -0300

    Arquivo que contém toda a regra de negócio

---
+++ src/main/java/com/union/coordenador/CoordenadorService.java
@@ -0,0 +1,36 @@
+package com.union.coordenador;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoordenadorService {
+	
+	private final CoordenadorRepository coordenadorRepository; // digamos que o repository é o db
+	
+	@Autowired // injetando uma instância do repositorio aqui ( injeção direta ? )
+	public CoordenadorService(CoordenadorRepository coordenadorRepository) {
+		super();
+		this.coordenadorRepository = coordenadorRepository;
+	}
+	
+	// GET
+	public List<Coordenador> pegarCoordenadores() {
+		return coordenadorRepository.findAll();
+	}
+	
+	// POST
+	public void adicionarNovoCoordenador(Coordenador coordenador) {
+		Optional<Coordenador> coordenadorOptional = coordenadorRepository.findCoordenadorByNome(coordenador.getNome());
+		
+		if (coordenadorOptional.isPresent()) {
+			throw new IllegalStateException("nome já cadastrado");
+		}
+		
+		coordenadorRepository.save(coordenador);
+	}
+	
+}
