d65044f57406fcdb3247767378393ee75ba603a4
renans04 <renansantos.silva04@outlook.com>
2021-09-29 21:28:14 -0300

    Classe Funcionario com relacionamento ManyToOne

--- src/main/java/com/union/funcionario/Funcionario.java
+++ src/main/java/com/union/funcionario/Funcionario.java
@@ -1,5 +1,101 @@
 package com.union.funcionario;
 
-public class Funcionario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import com.union.coordenador.Coordenador;
+
+@Entity
+@Table(name = "Funcionario")
+public class Funcionario{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nome;
+	private String cpf;
+	private String cep;
+	private Integer telefone;
+	
+	// private Integer codigoPerfil;
+
+	@ManyToOne
+	@JoinColumn(name = "coordenadorId", referencedColumnName = "id")
+	private Coordenador coordenador;
+	
+	public Funcionario() {
+		super();
+	}
+	
+	public Funcionario(Integer id, String nome, String cpf, String cep, Integer telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.cep = cep;
+		this.telefone = telefone;
+		// this.codigoPerfil = codigoPerfil;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+
+	/*
+	public Integer getCodigoPerfil() {
+		return codigoPerfil;
+	}
+
+	public void setCodigoPerfil(Integer codigoPerfil) {
+		this.codigoPerfil = codigoPerfil;
+	}
+	*/
+	
+	
 }
