f53e31b90ca050aae3d0d6b26efd9a6d53a10438
renans04 <renansantos.silva04@outlook.com>
2021-09-29 21:37:24 -0300

    Repository de Coordenador atualizado, foi removido alguns comentários

--- src/main/java/com/union/repository/CoordenadorRepository.java
+++ src/main/java/com/union/repository/CoordenadorRepository.java
@@ -9,7 +9,6 @@ import com.union.coordenador.Coordenador;
 
 @Repository // responsável por interagir com o db
 public interface CoordenadorRepository extends JpaRepository<Coordenador, Integer>{
-	// não entendi ainda o que é uma interface, mas acredito que seja onde duas ou mais classes possam ter o mesmo método
 	
 	// @Query("SELECT s FROM Student s WHERE s.email = ?1") copiei de um antigo código e não entendi
 	Optional<Coordenador>findCoordenadorByNome(String nome);
