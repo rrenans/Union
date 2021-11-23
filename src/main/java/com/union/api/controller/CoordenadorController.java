de17466cf0a5c21045a733b5c6f3ffd3ecdf4148
marzeu <marzahell@hotmail.com>
2021-11-22 23:30:22 -0300

    autenticaçao basica de coordenador

---
+++ src/main/java/com/union/api/controller/CoordenadorController.java
@@ -0,0 +1,47 @@
+package com.union.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+import com.union.api.dto.CoordenadorDTO;
+import com.union.exceptions.ErroAutenticacao;
+import com.union.exceptions.RegraNegocioException;
+import com.union.model.entity.Coordenador;
+import com.union.service.CoordenadorService;
+
+@RestController
+@RequestMapping("/api/coordenadores")
+@RequiredArgsConstructor
+public class CoordenadorController {
+
+	private final CoordenadorService coordenadorService;
+
+	@PostMapping("/autenticar")
+	public ResponseEntity<Object> autenticar(@RequestBody CoordenadorDTO dto) {
+		try {
+			Coordenador coordenadorAutenticado = coordenadorService.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(coordenadorAutenticado);
+		} catch (ErroAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Object> salvar(@RequestBody CoordenadorDTO dto) {
+
+		Coordenador coordenador = Coordenador.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha())
+				.build();
+
+		try {
+			Coordenador coordenadorSalvo = coordenadorService.salvarCoordenador(coordenador);
+			return new ResponseEntity<Object>(coordenadorSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+}
--- src/main/java/com/union/model/repository/CoordenadorRepository.java
+++ src/main/java/com/union/model/repository/CoordenadorRepository.java
@@ -1,22 +1,11 @@
 package com.union.model.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.union.model.entity.Coordenador;
 
-@Repository // responsável por interagir com o db
-public interface CoordenadorRepository extends JpaRepository<Coordenador, Integer> {
 
-	// @Query("SELECT s FROM Student s WHERE s.email = ?1") copiei de um antigo
-	// código e não entendi
-	/*
-	 * Optional<Coordenador> findCoordenadorByNome(String nome);
-	 * 
-	 * Coordenador findCoordenadorById(Integer id);
-	 */
+public interface CoordenadorRepository extends JpaRepository<Coordenador, Integer> {
 	
 	boolean existsByEmail(String email);
 
