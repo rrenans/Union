5f92d624a19864271198ab92c60786dafe93c314
marzeu <marzahell@hotmail.com>
2021-11-24 22:30:41 -0300

    auth em memoria

--- src/main/java/com/union/config/SecurityConfig.java
+++ src/main/java/com/union/config/SecurityConfig.java
@@ -1,20 +1,31 @@
 package com.union.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		String senhaCodificada = passwordEncoder().encode("abobora");
+		
+		auth.inMemoryAuthentication().withUser("admin").password(senhaCodificada).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();		
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 }
