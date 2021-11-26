a46d4c0234e3301cc59180689a391f9ff07def02
marzeu <marzahell@hotmail.com>
2021-11-25 21:19:38 -0300

    finalizado a autenticacao com jwt

--- src/main/java/com/union/config/SecurityConfig.java
+++ src/main/java/com/union/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.union.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,14 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.union.api.JwtTokenFilter;
+import com.union.service.JwtService;
 import com.union.service.impl.SecurityUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private SecurityUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtService jwtService;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -25,43 +32,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return encoder;
 	}
 
+	@Bean
+	public JwtTokenFilter jwtTokenFilter() {
+		return new JwtTokenFilter(jwtService, userDetailsService);
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.userDetailsService(userDetailsService)
-		.passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/api/coordenador/autenticar").permitAll()		
-		.antMatchers(HttpMethod.POST, "/api/coordenador").permitAll()		
-		.anyRequest().authenticated()
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.httpBasic();
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/api/coordenador/autenticar")
+				.permitAll().antMatchers(HttpMethod.POST, "/api/coordenador").permitAll().anyRequest().authenticated()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
