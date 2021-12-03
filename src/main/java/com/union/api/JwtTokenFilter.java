83be2908d6f30407273191646ea59e2ca0728412
renans04 <renansantos.silva04@outlook.com>
2021-12-02 21:58:16 -0300

    correção de merge

--- src/main/java/com/union/api/JwtTokenFilter.java
+++ src/main/java/com/union/api/JwtTokenFilter.java
@@ -33,10 +33,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
 		String authorization = request.getHeader("Authorization");
 
-		if(request.getRequestURI().contains("api/funcionarios")) {
-			filterChain.doFilter(request, response);
-			return ;
-		}
+		
 		
 		if (authorization != null && authorization.startsWith("Bearer")) {
 			String token = authorization.split(" ")[1];
